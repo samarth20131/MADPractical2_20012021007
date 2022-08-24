ac333b75bfb303f261f25174cba218237589563f
samarth20131 <samrajgupta3110@gmail.com>
2022-08-25 00:01:05 +0530

    practical 2 completed

--- app/src/main/java/com/example/madpractical2_20012021007/MainActivity.kt
+++ app/src/main/java/com/example/madpractical2_20012021007/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mycl=findViewById(R.id.mycl)
+        mycl=findViewById(R.id.myCoordinatorLayout)
         Log.i(TAG, "onCreate: onCreate method is called")
         Toast.makeText(this,"onCreate: onCreate method is called",Toast.LENGTH_LONG).show()
         Snackbar.make(mycl,"onCreate method is called",Snackbar.LENGTH_LONG).show()
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         setContentView(R.layout.activity_main)
-        mycl=findViewById(R.id.mycl)
+        mycl=findViewById(R.id.myCoordinatorLayout)
         Log.i(TAG, "onStart: onStart method is called")
         Toast.makeText(this,"onStart: onStart method is called",Toast.LENGTH_LONG).show()
         Snackbar.make(mycl,"onStart method is called",Snackbar.LENGTH_LONG).show()
@@ -31,7 +31,7 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         setContentView(R.layout.activity_main)
-        mycl=findViewById(R.id.mycl)
+        mycl=findViewById(R.id.myCoordinatorLayout)
         Log.i(TAG, "onResume: onResume method is called")
         Toast.makeText(this,"onResume: onResume method is called",Toast.LENGTH_LONG).show()
         Snackbar.make(mycl,"onResume method is called",Snackbar.LENGTH_LONG).show()
@@ -40,7 +40,7 @@ class MainActivity : AppCompatActivity() {
     override fun onPause() {
         super.onPause()
         setContentView(R.layout.activity_main)
-        mycl=findViewById(R.id.mycl)
+        mycl=findViewById(R.id.myCoordinatorLayout)
         Log.i(TAG, "onPause: onPause method is called")
         Toast.makeText(this,"onPause: onPause method is called",Toast.LENGTH_LONG).show()
         Snackbar.make(mycl,"onPause method is called",Snackbar.LENGTH_LONG).show()
@@ -49,16 +49,27 @@ class MainActivity : AppCompatActivity() {
     override fun onStop() {
         super.onStop()
         setContentView(R.layout.activity_main)
-        mycl=findViewById(R.id.mycl)
+        mycl=findViewById(R.id.myCoordinatorLayout)
         Log.i(TAG, "onStop: onStop method is called")
         Toast.makeText(this,"onStop: onStop method is called",Toast.LENGTH_LONG).show()
         Snackbar.make(mycl,"onStop method is called",Snackbar.LENGTH_LONG).show()
     }
 
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG, "onRestart:OnRestart method is called ")
+        Toast.makeText(this,"OnRestart:OnRestart method is called",Toast.LENGTH_LONG).show()
+        Snackbar.make(
+            findViewById(R.id.myCoordinatorLayout),
+            "onRestart method is called",
+            Snackbar.LENGTH_SHORT
+        ).show()
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         setContentView(R.layout.activity_main)
-        mycl=findViewById(R.id.mycl)
+        mycl=findViewById(R.id.myCoordinatorLayout)
         Log.i(TAG, "onDestroy: onDestroy method is called")
         Toast.makeText(this,"onDestroy: onDestroy method is called",Toast.LENGTH_LONG).show()
         Snackbar.make(mycl,"onDestroy method is called",Snackbar.LENGTH_LONG).show()
